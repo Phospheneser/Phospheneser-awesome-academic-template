074207b1d06eb33658043b5025b1119e2693bee0
Phospheneser <2388840666@qq.com>
2025-09-28 00:49:29 +0800

    fix: burger button now useful

--- static/js/index.js
+++ static/js/index.js
@@ -22,12 +22,66 @@ function setInterpolationImage(i) {
 
 $(document).ready(function() {
     // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function() {
+    $(".navbar-burger").click(function(e) {
+      console.log("=== 汉堡菜单被点击了！ ===");
+      console.log("点击目标：", e.target);
+      console.log("当前事件：", e);
+
       // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
       $(".navbar-burger").toggleClass("is-active");
       $(".navbar-menu").toggleClass("is-active");
 
+      console.log("切换后的状态：");
+      console.log("navbar-burger是否有is-active类：", $(".navbar-burger").hasClass("is-active"));
+      console.log("navbar-menu是否有is-active类：", $(".navbar-menu").hasClass("is-active"));
+
+      // 检查菜单项
+      const menuItems = $(".navbar-menu.is-active .navbar-item");
+      console.log("找到的菜单项数量：", menuItems.length);
+      if (menuItems.length > 0) {
+        const menuRect = $(".navbar-menu.is-active").get(0).getBoundingClientRect();
+        const itemRect = menuItems.first().get(0).getBoundingClientRect();
+        console.log("菜单尺寸：", {
+          width: menuRect.width,
+          height: menuRect.height,
+          top: menuRect.top,
+          left: menuRect.left
+        });
+        console.log("菜单项尺寸：", {
+          width: itemRect.width,
+          height: itemRect.height,
+          top: itemRect.top,
+          left: itemRect.left
+        });
+      }
+
+      // 防止默认行为
+      e.preventDefault();
+      return false;
     });
+
+    // 添加调试信息
+    console.log("页面加载完成，找到的.navbar-burger元素：", $(".navbar-burger").length);
+    console.log("找到的.navbar-menu元素：", $(".navbar-menu").length);
+
+    // 检查汉堡按钮定位
+    const burger = $(".navbar-burger").first();
+    if (burger.length > 0) {
+      const rect = burger.get(0).getBoundingClientRect();
+      const navbarBrand = $(".navbar-brand");
+      const brandRect = navbarBrand.get(0).getBoundingClientRect();
+      console.log("汉堡按钮位置：", {
+        left: rect.left,
+        right: rect.right,
+        width: rect.width,
+        top: rect.top
+      });
+      console.log("导航栏位置：", {
+        left: brandRect.left,
+        right: brandRect.right,
+        width: brandRect.width
+      });
+    }
 
     var options = {
 			slidesToScroll: 1,
